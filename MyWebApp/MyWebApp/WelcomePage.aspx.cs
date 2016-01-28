4ac8595ff886606c14d5a267273e59b940546042
tobenothing <tobenothing@126.com>
2016-01-28 18:46:27 +0800

    add new page

--- MyWebApp/MyWebApp/WelcomePage.aspx.cs
+++ MyWebApp/MyWebApp/WelcomePage.aspx.cs
@@ -7,13 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace MyWebApp
 {
-    public partial class ContactUs : System.Web.UI.Page
+    public partial class WelcomePage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-
-       
     }
 }
