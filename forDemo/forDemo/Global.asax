08ca957cb43d464aaea51359956e77605533108a
tobenothing <tobenothing@126.com>
2016-01-29 11:40:45 +0800

    create new project, need update

---
+++ forDemo/forDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="forDemo.Global" Language="C#" %>
---
+++ forDemo/forDemo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(forDemo.Startup))]
+namespace forDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
